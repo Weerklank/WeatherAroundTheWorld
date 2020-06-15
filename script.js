4b0026a698f6c10e9e6bd81ce1fc411569133b4d
Weerklank <Zoe.veggie@gmail.com>
2020-06-15 01:22:22 -0700

    added forecast cards

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
     let input = $('.form-control')
+    let one = null
+    let two = null
 
     loadCity()
 
@@ -9,11 +11,14 @@ $(document).ready(function () {
 
         if (isNaN(parseInt(city)) === true) {
 
+            console.log('shit fuck damn')
+            one = localStorage.getItem('city')
+            two = localStorage.getItem('zip')
             window.localStorage.removeItem('zip')
             window.localStorage.setItem('city', city)
 
             let settings = {
-                "url": 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+                "url": 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
                 "method": "GET",
                 "timeout": 0
             };
@@ -21,7 +26,7 @@ $(document).ready(function () {
             ajaxCurrent(settings)
 
             settings = {
-                "url": 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+                "url": 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
             };
 
             ajaxForcast(settings)
@@ -29,22 +34,41 @@ $(document).ready(function () {
 
         } else if (city.length === 5 && isNaN(parseInt(city)) === false) {
 
+            console.log('damn fuck shit')
+
+            one = localStorage.getItem('city')
+            two = localStorage.getItem('zip')
             window.localStorage.removeItem('city')
             window.localStorage.setItem('zip', city)
 
             let settings = {
-                "url": 'http://api.openweathermap.org/data/2.5/weather?zip=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+                "url": 'https://api.openweathermap.org/data/2.5/weather?zip=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
                 "method": "GET",
                 "timeout": 0
             };
 
             ajaxCurrent(settings)
+
+            settings = {
+                "url": 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+            };
+
+            ajaxForcast(settings)
+        } else {
+            console.log(one, two)
+            if (one === null) {
+                city = two
+                window.localStorage.setItem('zip', city)
+            } else if (two === null) {
+                city = one
+                window.localStorage.setItem('city', city)
+            }
+            confirm('Please enter a new city or zip, the one you entered does not exist in the database')
         }
     })
 
     function ajaxCurrent(settings) {
-        $.ajax(settings).then(function (response) {
-            console.log(response);
+        $.ajax(settings).done(function (response) {
             $('#cityName').text(response.name)
             $('#cityCountry').text(response.sys.country)
             $('#cityWeather').text(response.weather[0].description)
@@ -55,7 +79,7 @@ $(document).ready(function () {
             let lon = response.coord.lon
 
             settings = {
-                "url": 'http://api.openweathermap.org/data/2.5/uvi?appid=414413edc95ac55baac0a67078ade8d2&lat=' + lat + '&lon=' + lon,
+                "url": 'https://api.openweathermap.org/data/2.5/uvi?appid=414413edc95ac55baac0a67078ade8d2&lat=' + lat + '&lon=' + lon,
             };
 
             $.ajax(settings).then(function (response) {
@@ -72,42 +96,65 @@ $(document).ready(function () {
                     $('#cityUV').css('background-color', 'violet')
                 }
             })
+        }).fail(function (response) {
+            console.log(one, two)
+            if (one === null) {
+                city = two
+                window.localStorage.setItem('zip', city)
+            } else if (two === null) {
+                city = one
+                window.localStorage.setItem('city', city)
+            }
+            confirm('Please enter a new city or zip, the one you entered does not exist in the database')
         })
     }
 
     function ajaxForcast(settings) {
         $.ajax(settings).then(function (response) {
-            console.log(response);
+            console.log(response.list);
+            let j = 1
+            for (let i = 0; i < response.list.length; i++) {
+                if (response.list[i].dt_txt.includes('12:00:00')) {
+                    $('#date' + j).text((new Date(response.list[i].dt_txt).toLocaleDateString()))
+                    $('.temp' + j).text(response.list[i].main.temp)
+                    console.log(response.list[i].main.temp)
+                    $('.humid' + j).text(response.list[i].main.humidity)
+                    j++
+                    console.log(j)
+                }
+            }
             // $('#cityName').text(response.name)
-            // $('#cityCountry').text(response.sys.country)
-            // $('#cityWeather').text(response.weather[0].description)
-            // $('#cityTemp').text(response.main.temp)
-            // $('#cityHumid').text(response.main.humidity)
-            // $('#cityWind').text(response.wind.speed)
-            // let lat = response.coord.lat
-            // let lon = response.coord.lon
-            })
-        }
+        })
+    }
 
     function loadCity() {
         city = localStorage.getItem('city')
         zip = localStorage.getItem('zip')
         if (zip === null) {
             let settings = {
-                "url": 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+                "url": 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
                 "method": "GET",
                 "timeout": 0
             };
             ajaxCurrent(settings)
-        }
-        else if (city === null) {
+            settings = {
+                "url": 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+            };
+
+            ajaxForcast(settings)
+        } else if (city === null) {
             city = zip
             let settings = {
-                "url": 'http://api.openweathermap.org/data/2.5/weather?zip=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+                "url": 'https://api.openweathermap.org/data/2.5/weather?zip=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
                 "method": "GET",
                 "timeout": 0
             };
             ajaxCurrent(settings)
+            settings = {
+                "url": 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=414413edc95ac55baac0a67078ade8d2&units=imperial',
+            };
+
+            ajaxForcast(settings)
         }
     }
 
